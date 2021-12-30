3296e7fdc4a0e6118c856bd6f3f4b4a72f65d87b
Caleb <beerscb@gmail.com>
2021-12-30 11:40:11 -0500

    add file-writing functionality to context

--- lib/scrapexer.ex
+++ lib/scrapexer.ex
@@ -6,4 +6,48 @@ defmodule Scrapexer do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def create_directory_name(url) do
+    url
+    |> PageScrape.base_url
+    |> String.split("www.")
+    |> tl
+    |> hd
+    |> String.split(".")
+    |> hd
+
+  end
+
+  def create_html_file_name(url) do
+    filename = url
+    |> String.split(".org")
+    |> tl
+    |> hd
+    |> String.replace(~r/\//,"_") #replace slashes with underscores to avoid directory problems
+
+    case filename do
+      "" -> "main.html"
+      _ -> filename
+    end
+
+  end
+
+  def writefile(url) do
+    directory = create_directory_name(url)
+    filename = create_html_file_name(url)
+    path = directory <> "/" <> filename
+    text = PageScrape.html_as_string(url)
+
+    File.mkdir(directory)
+    {:ok, file} = File.open(path, [:write,:read,:utf8])
+
+    IO.puts(file, text)
+  end
+
+  def write_all(url) do
+    Spider.domain_crawler(url)
+    |> Enum.map(fn x -> writefile(x) end)
+
+  end
+
 end
