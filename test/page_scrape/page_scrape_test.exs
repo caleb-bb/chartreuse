557d9ef5f57d572bec2c9941d02333ae5c1612d6
Caleb <beerscb@gmail.com>
2022-01-05 15:36:59 -0500

    add data for collected domain links, test coverage for domain_links_from_html

--- test/page_scrape/page_scrape_test.exs
+++ test/page_scrape/page_scrape_test.exs
@@ -7,11 +7,22 @@ defmodule PageScrapeTest do
 
     expected_links = String.split(collected_links,"\n")
 
+    {:ok, html_links} = File.read("test/resources/page_scrape/main.html")
+    actual_links = links_from_html(html_links)
+
+    assert expected_links == actual_links
+  end
+
+  test "domain_links_from_html" do
+
+    {:ok, collected_links} = File.read("test/resources/page_scrape/collected_domain_links")
+    expected_links = String.split(collected_links,"\n")
+    |> Enum.drop(-1)
+
 
     {:ok, html_links} = File.read("test/resources/page_scrape/main.html")
-
-    actual_links = links_from_html(html_links)
-    |> IO.inspect
+    domain = "https://www.hopelutheransunbury.org"
+    actual_links = domain_links_from_html(html_links,domain)
 
     assert expected_links == actual_links
   end
